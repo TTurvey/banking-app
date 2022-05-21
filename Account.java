88c46d49367b34d0af8f0444dc4669829054a4ff
Tim Turvey <tturvey@hotmail.co.uk>
2022-05-21 12:40:54 +0100

    Empty methods setup

---
+++ Account.java
@@ -0,0 +1,21 @@
+public class Account {
+
+    public int balance;
+    
+    public Account(int initialBalance) {
+        
+    }
+
+    public void Deposit(int amount) {
+        
+    }
+
+    public void Withdraw(int amount) {
+    }
+
+    public string PrintStatement()
+    {
+        return "";
+    }
+
+}
